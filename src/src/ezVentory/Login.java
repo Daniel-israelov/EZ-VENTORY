8acde858e91eb7fff5d915508d5a1a9f04a8e7e6
Anav <idanlol99@gmail.com>
2022-01-06 16:12:51 +0200

    changed the login page, and the employee class

--- src/src/ezVentory/Login.java
+++ src/src/ezVentory/Login.java
@@ -3,13 +3,35 @@ package src.ezVentory;
 import java.util.HashMap;
 
 public class Login {
-    HashMap<String,String> loginInfo = new HashMap<String,String>();
+
+    Store store;
+
+    private int systemMode;
+
+    public void setSystemMode(int systemMode) {
+        this.systemMode = systemMode;
+    }
+
+    public boolean validation(Employee emp){
+        for(Employee employee:store.getEmployees()){
+            if(employee.equals(emp)){
+                setSystemMode(emp.getAccessType());
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /*
+     HashMap<String,String> loginInfo = new HashMap<String,String>();
     Login(){
-        loginInfo.put("employee1","123");
-        loginInfo.put("employee2","1234");
-        loginInfo.put("employee3","12345");
+        loginInfo.put("admin","123");
+
     }
     protected HashMap getLoginInfo(){
         return loginInfo;
     }
+
+     */
 }
