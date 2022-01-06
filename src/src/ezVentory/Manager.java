5152ea3c650797846387dec095cd08bc5f851538
Daniel Israelov <svhrkz@gmail.com>
2022-01-06 17:55:08 +0200

    fixed constructor

--- src/src/ezVentory/Manager.java
+++ src/src/ezVentory/Manager.java
@@ -1,31 +1,27 @@
 package src.ezVentory;
 
-public class Manager extends Staff{
+public class Manager extends Staff {
     public Store store;
-    public Manager(String firstName, String lastName, String id, String password, int accessType) {
-        super(firstName, lastName, id, password, accessType);
+
+    public Manager(String firstName, String lastName, String id, String password, int accessType, String roleName) {
+        super(firstName, lastName, id, password, accessType, roleName);
     }
 
-    public void createOrder(){
-
-    }
-
-    public void addEmployee(){
+    public void createOrder() {
 
     }
 
-    public boolean removeEmployee(Employee emp){
-        for(Employee employee : store.getEmployees() ){
-            if(employee.equals(emp)){
+    public void addEmployee() {
+
+    }
+
+    public boolean removeEmployee(Employee emp) {
+        for (Employee employee : store.getEmployees()) {
+            if (employee.equals(emp)) {
                 store.removeEmployee(employee);
                 return true;
             }
         }
         return false;
     }
-
-
-
-
-
-
+}
