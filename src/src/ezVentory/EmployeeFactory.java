156246f5b95ceeb1044b2ad73a7ca7c6feea7ce9
Daniel Israelov <svhrkz@gmail.com>
2022-01-06 17:51:16 +0200

    updated factory

--- src/src/ezVentory/EmployeeFactory.java
+++ src/src/ezVentory/EmployeeFactory.java
@@ -1,17 +1,31 @@
 package src.ezVentory;
 
+import java.util.Scanner;
+
 public class EmployeeFactory {
     public Employee getEmployee(String employeeType){
         if(employeeType == null)
             return null;
 
+        Scanner sc = new Scanner(System.in);
+        String firstName, lastName, id, password, roleName;
+        int accessType;
+
+        firstName = sc.next();
+        lastName = sc.next();
+        id = sc.next();
+        password = sc.next();
+        accessType = sc.nextInt();
+        roleName = sc.next();
+
         if(employeeType.equalsIgnoreCase("STAFF"))
-            return new Staff();
+            return new Staff(firstName, lastName, id, password, accessType, roleName);
+
 
         if(employeeType.equalsIgnoreCase("STORE MANAGER"))
-            return new Manager();
+            return new Manager(firstName, lastName, id, password, accessType, roleName);
 
-        if(employeeType.equalsIgnoreCase("SHIFT MANAGER"))
+/*        if(employeeType.equalsIgnoreCase("SHIFT MANAGER"))
             return new ShiftManager();
 
         if(employeeType.equalsIgnoreCase("STORAGE MANAGER"))
@@ -19,9 +33,10 @@ public class EmployeeFactory {
 
         if(employeeType.equalsIgnoreCase("STORE KEEPER"))
             return new StoreKeeper();
+*/
 
         if(employeeType.equalsIgnoreCase("CASHIER"))
-            return new Cashier();
+            return new Cashier(firstName, lastName, id, password, accessType, roleName);
 
         return null;
     }
