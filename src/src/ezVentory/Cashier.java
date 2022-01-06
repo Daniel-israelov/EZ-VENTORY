413867ae0bd97057060559e6b30c5711c51c1922
Daniel Israelov <svhrkz@gmail.com>
2022-01-06 17:50:48 +0200

    fixed constructor

--- src/src/ezVentory/Cashier.java
+++ src/src/ezVentory/Cashier.java
@@ -1,7 +1,7 @@
 package src.ezVentory;
 
 public class Cashier extends Staff{
-    public Cashier(String firstName, String lastName, String id, String password, int accessType) {
-        super(firstName, lastName, id, password, accessType);
+    public Cashier(String firstName, String lastName, String id, String password, int accessType, String roleName) {
+        super(firstName, lastName, id, password, accessType, roleName);
     }
 }
