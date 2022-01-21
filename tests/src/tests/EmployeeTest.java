e0f30d3ca281dcbc45ca798c2d9b1781301d218b
Anav <idanlol99@gmail.com>
2022-01-21 12:56:28 +0200

    test the test

---
+++ tests/src/tests/EmployeeTest.java
@@ -0,0 +1,62 @@
+package src.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getAccessType() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void create() {
+    }
+}
